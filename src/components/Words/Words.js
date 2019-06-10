385c2c74395d84c16d4aa2600109d16b6ec999fd
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-10 13:30:51 -0500

    very basic Words component functioning

---
+++ src/components/Words/Words.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Words.css";
+
+function Words() {
+    return (
+      <div className="Words">
+        <header className="header">
+        </header>
+        <h1>Click Game</h1>
+        <h2>CLick on an image to score a point but be careful not to click the same image twice!</h2>
+      </div>
+    );
+  }
+  
+  export default Words;
+  
