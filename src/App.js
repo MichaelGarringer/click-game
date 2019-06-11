607db368c498bcfe153a780dd5c0bc72c17dd5e0
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-11 12:58:36 -0500

    game is fully function with the cards displaying randomly when the game starts, score also increments when a new image is clicked - game is won when player reaches 12 points. future update will make a victory more obvious than the simple alert that is currently displayed

--- src/App.js
+++ src/App.js
@@ -9,18 +9,69 @@ import dbz from "../src/dbz.json"
 class App extends Component {
   state = {
     dbz,
-    score: 0,
-  }
+    clicked: [],
+    score: 0
+  };
+
+  //when you click on a card ... the fish is taken out of the array
+  imageClick = event => {
+    const zfighter = event.target.alt;
+    const alreadyClicked =
+      this.state.clicked.indexOf(zfighter) > -1;
+
+    //if you click on a fish that has already been selected, the game is reset and cards reordered
+    if (alreadyClicked) {
+      this.setState({
+        dbz: this.state.dbz.sort(function (a, b) {
+          return 0.5 - Math.random();
+        }),
+        clicked: [],
+        score: 0
+      });
+      alert("You already clicked that one, pal");
+
+      //if you click on an available fish, your score is increased and cards reordered
+    } else {
+      this.setState(
+        {
+          dbz: this.state.dbz.sort(function (a, b) {
+            return 0.5 - Math.random();
+          }),
+          clicked: this.state.clicked.concat(
+            zfighter
+          ),
+          score: this.state.score + 1
+        },
+        //if you get all 12 fish corrent you get a congrats message and the game resets        
+        () => {
+          if (this.state.score === 12) {
+            alert("You did it");
+            this.setState({
+              dbz: this.state.dbz.sort(function (a, b) {
+                return 0.5 - Math.random();
+              }),
+              clicked: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+  
+
   render() {
     return (
       <div>
         <Words />
         <div className="scores">
-          <Scores />
+          <Scores score={this.state.score}
+          />
         </div>
         <div className="container">
           {this.state.dbz.map(dbz => (
             <Cards
+              imageClick={this.imageClick}
               id={dbz.id}
               key={dbz.key}
               image={dbz.image}
--- src/components/Scores/Scores.js
+++ src/components/Scores/Scores.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Scores.css";
 
 //pass the image into each card so all 12 are rendered
-function Scores(){
+class Scores extends Component{
+    render(){
     return(
-    <h1>Score will be displayed here once I figure it out :)</h1>
-    )
+    <h1>Score: {this.props.score}</h1>
+    )}
 }
 export default Scores;
