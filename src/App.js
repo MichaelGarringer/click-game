7c607f48926d024af78ec256bbac3c88f1cde5fd
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-10 20:28:43 -0500

    images now displaying but display itself still needs work, images still cannot be clicked and do not generate randomly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import Words from './components/Words';
+import Cards from './components/Cards';
+import dbz from "../src/dbz.json"
+
 
 class App extends Component {
-  
+state ={
+  dbz
+}
   render() {
     return (
       <div>
         <Words />
+        <div className="container">
+          {this.state.dbz.map(dbz => (
+            <Cards
+              image={dbz.image}
+            />
+          ))}
+        </div>
       </div>
+      
     )
   }
 }
+
 
 export default App;
--- src/components/Words/Words.js
+++ src/components/Words/Words.js
@@ -5,8 +5,8 @@ function Words() {
     return (
      
         <header className="header">
-        <h1>Click Game</h1>
-        <h2>CLick on an image to score a point but be careful not to click the same image twice!</h2>
+        <h1>DBZ Click Game</h1>
+        <h2>Click on an image to score a point but be careful not to click the same image twice!</h2>
       </header>
     );
   }
